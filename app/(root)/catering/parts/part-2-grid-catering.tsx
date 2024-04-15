1123d882b982a04e1d1909d166a09efc4f915c66
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-04-15 04:51:33 -0600

    caterign styles

--- app/(root)/catering/parts/part-2-grid-catering.tsx
+++ app/(root)/catering/parts/part-2-grid-catering.tsx
@@ -1,22 +1,19 @@
 import { comida1 } from "@/assets";
 import { cn } from "@/utils/utils";
 import Image from "next/image";
+import "@/styles/catering-styles.css";
+
 interface Part2GridCateringProps {
   classname?: string;
 }
 
 export const Part2GridCatering = ({ classname }: Part2GridCateringProps) => {
   return (
-    <div
-      className={cn(
-        "grid gap-5 grid-rows-3 grid-cols-2 2xl:grid-rows-3 2xl:grid-cols-3  ",
-        classname
-      )}
-    >
+    <div className={cn("gridcatering gap-5   ", classname)}>
       <Image
         src={comida1}
         alt="catering"
-        className="rounded-3xl  col-start-1 col-end-3 row-start-1 row-end-3 "
+        className="rounded-3xl  col-start-1 col-end-3 row-start-1 row-end-3 w-[100%] h-[100%]"
       />
       <div className="cristalblur rounded-3xl ">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum,
@@ -43,3 +40,5 @@ export const Part2GridCatering = ({ classname }: Part2GridCateringProps) => {
     </div>
   );
 };
+
+//note: if your are using taildwindcss using grid is going to be harderd.
--- app/(root)/catering/sections-catering/hero-grid-section.tsx
+++ app/(root)/catering/sections-catering/hero-grid-section.tsx
@@ -1,3 +1,5 @@
+'use server';
+
 import { comida11 } from "@/assets";
 import Image from "next/image";
 import { Part1Catering } from "../parts/part-1-catering";
@@ -6,12 +8,12 @@ import { Part2GridCatering } from "../parts/part-2-grid-catering";
 export const HeroGridSection = () => {
   return (
     <section className=" flex flex-col xl:flex-row relative  ">
-        <Part1Catering classname="xl:basis-1/2 m-5" />
-        <Part2GridCatering classname="xl:basis-1/2 m-5 " />
+        <Part1Catering classname="basis-1/2 m-5" /> 
+        <Part2GridCatering classname="basis-1/2 m-5 " />
 
-
-      <div className="absolute -z-10 w-full h-full">
-        <Image src={comida11} alt={"Comida11"}  className="w-full h-full"/>
+      {/* Esto es para [poner un aimagen de fondo con nextjs. Es un poco complejo y basico  */}
+      <div className="absolute -z-10 w-full h-full"> 
+        <Image src={comida11} alt={"Comida11"}  className="w-full h-full object-cover"/>
       </div>
     </section>
   );
