ca8808de80d968f9d27370587585da5c7f79c3ce
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-05-14 23:56:10 -0600

    funcionalidad de la navbar

--- app/(root)/menu/page.tsx
+++ app/(root)/menu/page.tsx
@@ -3,11 +3,13 @@
 import { products } from "@/assets/dummydb";
 import { ListaCardsMenu } from "@/components/ui/menu/list-cards-menu";
 import { NavMenuSection } from "../sections/menu-section";
+import { useState } from "react";
 
 const Menu = () => {
+  const [selectedMenu, setSelectedMenu] = useState("");
   return (
     <main className="min-h-screen relative lg:pr-[20vw] md:pr-[30vw] xl:pr-[16vw] xxl:pr-[20vw]  xl:-px-0 ">
-      <NavMenuSection onclose={()=>{}}/>
+      <NavMenuSection select={()=>{setSelectedMenu}}/>
       <ListaCardsMenu
         products={products[0].milkshakes}
         titulo={" Super Milshakes"}
--- app/(root)/sections/menu-section.tsx
+++ app/(root)/sections/menu-section.tsx
@@ -2,14 +2,14 @@ import { menuSection } from "@/assets/constants";
 
 interface MenuSectionProps {
   showMenu?: boolean;
-  onclose: () => void;
+  select: () => string;
 }
 
-export const NavMenuSection = ({ showMenu, onclose }: MenuSectionProps) => {
+export const NavMenuSection = ({ showMenu, select }: MenuSectionProps) => {
   return (
     <nav className="w-full bg-primary h-36 flex items-center justify-center gap-16">
       {menuSection.map((item) => (
-        <button onClick={onclose} key={item.name} className="">
+        <button onClick={() => select(item.)} key={item.name} className="">
           <div className="flex flex-col items-center justify-center">
             <div className="text-5xl">{item.icon}</div>
             <div>{item.name}</div>
